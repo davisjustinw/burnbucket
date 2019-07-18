01b601be03eeaffa63a24b00f88291822080a7ef
davisjustinw <davisjustinw@gmail.com>
2019-07-17 21:30:07 -0700

    add error handling for journal entry, no bucket selected, not logged in, permission to edit

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -4,18 +4,38 @@ class JournalController < ApplicationController
   post '/journal' do
     #binding.pry
     # {"bucket"=>["7", "1"], "moment_input"=>"200 dangles #dangle invoice", "function"=>"submit"}
-    value_unit, note = params[:moment_input].split('#')
-    value, unit = value_unit.strip.split(' ', 2)
-    value = value.to_i
-    unit = Unit.find_or_create_by name: unit
 
-    params[:bucket].each do |bucket_id|
-      Bucket.find(bucket_id).tap do |bucket|
-        bucket.potential ? new_value = value * -1 : new_value = value
-        bucket.moments.create name: note, unit: unit, value: new_value
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+
+      if params[:bucket]
+        value_unit, note = params[:moment_input].split('#')
+        value, unit = value_unit.strip.split(' ', 2)
+        value = value.to_i
+
+        unit = Unit.find_or_create_by name: unit if unit
+
+        params[:bucket].each do |bucket_id|
+          if @user.buckets.find bucket_id
+            Bucket.find(bucket_id).tap do |bucket|
+              bucket.potential ? new_value = value * -1 : new_value = value
+              bucket.moments.create name: note, unit: unit, value: new_value
+              binding.pry
+            end
+          else
+            flash[:message] = "You don't have permission to edit this bucket"
+          end
+        end
+      else
+        flash[:message] = "No bucket selected"
       end
+
+      redirect '/buckets'
+
+    else
+      redirect '/login'
     end
 
-    redirect '/buckets'
+
   end
 end
