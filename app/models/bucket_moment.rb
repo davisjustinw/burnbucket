a6a5d7ee6f9c57b88f4cce269813eaa8c1b425e5
davisjustinw <davisjustinw@gmail.com>
2019-08-04 14:08:44 -0700

    pause for model rebuild

--- app/models/bucket_moment.rb
+++ app/models/bucket_moment.rb
@@ -1,4 +1,5 @@
 class BucketMoment < ActiveRecord::Base
-  belongs_to :bucket
+  belongs_to :to_bucket, classname: "bucket", foreign_key: "to_id"
+  belongs_to :from_bucket, classname: "bucket", foreign_key: "from_id"
   belongs_to :moment
 end
--- db/migrate/20190803220400_create_bucket_moments.rb
+++ db/migrate/20190803220400_create_bucket_moments.rb
@@ -2,9 +2,9 @@
 class CreateBucketMoments < ActiveRecord::Migration
   def change
     create_table :bucket_moments do |t|
-      t.integer :bucket_id
+      t.integer :to_id
+      t.integer :from_id
       t.integer :moment_id
-      t.integer :multiplier
     end
   end
 end
