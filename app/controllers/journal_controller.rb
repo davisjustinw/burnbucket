333eb4e61e7e68f124d40b53ed70bcc13ae3f9f7
davisjustinw <davisjustinw@gmail.com>
2019-08-12 21:47:33 -0700

    add simplified hournal entry

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -18,33 +18,19 @@ class JournalController < ApplicationController
 
 
   post '/journal' do
-    #binding.pry
-    # {"bucket"=>["7", "1"], "moment_input"=>"200 dangles #dangle invoice", "function"=>"submit"}
 
     if Helpers.is_logged_in?(session)
       @user = User.find(session[:user_id])
 
-        params[:moment][:unit] = Unit.find_or_create_by name: params[:moment][:unit]
-        params[:moment][:timestamp] = Time.now
-        new_moment = Moment.create params[:moment]
+      params[:moment][:timestamp] = Time.now
 
-        #binding.pry
-        params[:bucket].each do |function, bucket_id|
-          #binding.pry
-          user_bucket = @user.buckets.find bucket_id
-
-          if user_bucket
-            #new_moment.buckets << Bucket.find(bucket_id)
-            #BucketMoment.where bucket_id: bucket_id, moment_id: new_moment.id
-            multiplier = function == "to" ? 1 : -1
-            BucketMoment.create multiplier: multiplier, moment: new_moment, bucket: user_bucket
-            binding.pry
-          else
-            flash[:message] = "You don't have permission to edit this bucket"
-          end
-        end
-
-
+      user_bucket = @user.buckets.find params[:bucket]
+      binding.pry
+      if user_bucket
+        user_bucket.moments.create params[:moment]
+      else
+        flash[:message] = "You don't have permission to edit this bucket"
+      end
 
       redirect '/journal'
 
