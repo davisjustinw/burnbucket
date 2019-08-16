c212844dccb8c717fcc09651c1a33ca0a7b63fdd
davisjustinw <davisjustinw@gmail.com>
2019-08-15 21:16:12 -0700

    add edit bucket_list

--- app/controllers/bucket_list_controller.rb
+++ app/controllers/bucket_list_controller.rb
@@ -27,6 +27,38 @@ class BucketListController < ApplicationController
     end
   end
 
+  get '/bucket_lists/edit/:id' do
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+      @buckets = @user.buckets
+
+      @bucket_list = BucketList.find params[:id]
+
+      erb :'bucket_lists/edit_bucket_list'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/bucket_lists/:id' do
+    if Helpers.is_logged_in?(session)
+
+      @user = User.find(session[:user_id])
+      if @user.bucket_lists.exists?(params[:id])
+        @bucket_list = BucketList.find(params[:id])
+        @bucket_list.update(params[:bucket_list])
+        @bucket_list.bucket_ids = params[:buckets]
+      else
+        flash[:message] = "You do not have permission to edit this bucket or it does not exist"
+      end
+      redirect '/journal'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
 
 
 end
