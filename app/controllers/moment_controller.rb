cd4349eb2c9bf9619ecadebe8301bdd78d2d8605
davisjustinw <davisjustinw@gmail.com>
2019-08-25 09:04:44 -0700

    refactor moment controller with redirect_if_not_logged_in

--- app/controllers/moment_controller.rb
+++ app/controllers/moment_controller.rb
@@ -2,64 +2,56 @@ class MomentController < ApplicationController
 
   get '/moments/:id/edit' do
 
-    if Helpers.is_logged_in?(session)
-      @user = User.find(session[:user_id])
+    redirect_if_not_logged_in
+    @user = User.find(session[:user_id])
 
-      if @user.moments.exists?(params[:id])
-        @moment = Moment.find(params[:id])
-        @buckets = @user.buckets
-        erb :'moments/edit_moment'
-      else
-        flash[:message] = "You don't have persmission to edit this moment or it doesn't exist"
-        binding.pry
-        redirect '/journal'
-      end
+    if @user.moments.exists?(params[:id])
+      @moment = Moment.find(params[:id])
+      @buckets = @user.buckets
+      erb :'moments/edit_moment'
     else
-      redirect '/login'
+      flash[:message] = "You don't have persmission to edit this moment or it doesn't exist"
+      binding.pry
+      redirect '/journal'
     end
-  end
+
+  end # get moments
 
   patch '/moments/:id' do
 
-    if Helpers.is_logged_in?(session)
+    redirect_if_not_logged_in
 
-      @user = User.find(session[:user_id])
-      if @user.moments.exists?(params[:id]) && @user.buckets.exists?(params[:moment][:bucket])
-        @moment = Moment.find(params[:id])
+    @user = User.find(session[:user_id])
+    if @user.moments.exists?(params[:id]) && @user.buckets.exists?(params[:moment][:bucket])
+      @moment = Moment.find(params[:id])
 
-        params[:moment][:bucket] = Bucket.find(params[:moment][:bucket])
-        params[:moment][:timestamp] = Time.now
-
-      
-          @moment.update(params[:moment])
+      params[:moment][:bucket] = Bucket.find(params[:moment][:bucket])
+      params[:moment][:timestamp] = Time.now
 
 
-      else
-        flash[:message] = "You do not have permission to edit this moment or bucket, or they don't exist"
-      end
-      redirect '/journal'
+        @moment.update(params[:moment])
+
+
     else
-      redirect '/login'
+      flash[:message] = "You do not have permission to edit this moment or bucket, or they don't exist"
     end
-  end
+    redirect '/journal'
+
+  end #patch moment
 
   delete '/moments/:id' do
-    if Helpers.is_logged_in?(session)
+    redirect_if_not_logged_in
 
-      @user = User.find(session[:user_id])
-      if @user.moments.exists?(params[:id])
-        @moment = Moment.find(params[:id])
-        @moment.destroy
-        redirect '/journal'
-      else
-        flash[:message] = "You do not have permission to edit this moment or it does not exist"
-        redirect '/journal'
-      end
+    @user = User.find(session[:user_id])
+    if @user.moments.exists?(params[:id])
+      @moment = Moment.find(params[:id])
+      @moment.destroy
+      redirect '/journal'
     else
-      redirect '/login'
+      flash[:message] = "You do not have permission to edit this moment or it does not exist"
+      redirect '/journal'
     end
 
-  end
+  end #delte moment
 
-
-end
+end #journal controller
